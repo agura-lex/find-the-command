7843b65c302cbca6141fa1d85ffd7c45023b4905
Alexander Agura <agur4ik09@gmail.com>
2016-05-14 18:26:24 +0300

    Add non-interactive hook for bash

---
+++ usr/share/doc/find-my-command/fmc.bash
@@ -0,0 +1,16 @@
+command_not_found_handle() 
+{
+	local CMD=$1
+	local PKGS=$(pm -Foq /usr/bin/$CMD 2> /dev/null)
+	case $(echo $PKGS | wc -w) in
+		0) echo "$0: $CMD: command not found"
+			return 127 ;;
+		1) printf "\"$CMD\" may be found in package \"$PKGS\"\n" ;;
+		*)
+			printf "\"$CMD\" may be found in the following packages:\n"
+			for PKG in `echo -n $PKGS`
+			do
+			printf "\t$PKG\n"
+			done
+	esac
+}
